74c3c383edbd8f591c1c8f446dee40e8a77e4f55
namelesspark <jade.lake8852@gmail.com>
2026-07-28 09:50:51 +0900

    2026.07.28 9:50 study: 실습2 - f-string 안에서 계산하기

---
+++ Practice/03_06_example.py
@@ -0,0 +1,9 @@
+print("\n=== 실습 1 - f-string으로 변수 끼워 출력하기 ===")
+name = "PUMP-A"
+temp = 87
+print(f"설비: {name}, 온도: {temp}")
+
+
+print("\n=== 실습 2 - f-string 안에서 계산하기 ===")
+num1, num2, num3 = 72, 84, 120
+print(f"평균: {(num1 + num2 + num3) / 3}")
