de72b8894b9775a44e1813f8a6b1de5219b430ef
namelesspark <jade.lake8852@gmail.com>
2026-08-13 23:18:11 +0900

    2026.08.13 23:18 study(Python): Pandas 기초와 DataFrame 탐색 학습

---
+++ Python/24_Pandas.py
@@ -0,0 +1,12 @@
+import os, csv, numpy as np, pandas as pd
+
+file_path = os.path.join("Data", "12_metro_small.csv")
+
+try:
+    df = pd.read_csv(file_path, sep=",", index_col="모터전류", encoding="utf-8")
+except FileNotFoundError:
+    print(f"file not found:{file_path}")
+
+print(df.count(1))
+print("-" * 50)
+print(df.shape)
